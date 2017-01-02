9941c229a08bf45bf126ad2a6020efddab4fae1c
root <root@static.124.76.243.136.clients.your-server.de>
2017-01-03 00:43:06 +0100

    updated for Lineage updates

--- mirror_Z008.sh
+++ mirror_Z008.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 NOW=$(date +"%Y%m%d")
 cd /var/www/zenfone/Z008
-if [ ! -f cm-14.1-$NOW-UNOFFICIAL-Z008.zip ]; 
+if [ ! -f lineage-14.1-$NOW-UNOFFICIAL-Z008.zip ]; 
     then
     echo "File not found! Downloading the today's release."
-    wget http://theflamingskull.com/downloads/cm-14.1/Z008/cm-14.1-$NOW-UNOFFICIAL-Z008.zip
+    wget http://theflamingskull.com/downloads/cm-14.1/Z008/lineage-14.1-$NOW-UNOFFICIAL-Z008.zip
 else
 echo "You already got the today's release."
 fi
--- mirror_Z00A.sh
+++ mirror_Z00A.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 NOW=$(date +"%Y%m%d")
 cd /var/www/zenfone/Z00A
-if [ ! -f cm-14.1-$NOW-UNOFFICIAL-Z00A.zip ]; 
+if [ ! -f lineage-14.1-$NOW-UNOFFICIAL-Z00A.zip ]; 
     then
     echo "File not found! Downloading the today's release."
-    wget http://theflamingskull.com/downloads/cm-14.1/Z00A/cm-14.1-$NOW-UNOFFICIAL-Z00A.zip
+    wget http://theflamingskull.com/downloads/cm-14.1/Z00A/lineage-14.1-$NOW-UNOFFICIAL-Z00A.zip
 else
 echo "You already got the today's release."
 fi
